5b407fb44800f0c696b5ea1a76fbadf45f4c6f8f
Natanael Freire <natan.nascimento.freire@gmail.com>
2021-04-13 11:21:16 -0300

    header Access-Control-Allow-Origin authorization

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const apiAuthorization = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: 'Access-Control-Allow-Origin'
 });
 
 interface Response {
