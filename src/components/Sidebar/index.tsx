602be82ce10fd17f1e902a49c7150b821f44ee4c
Natanael Freire <natan.nascimento.freire@gmail.com>
2021-04-15 20:49:53 -0300

    adicionando um botão de logout

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiUsers, FiEdit, FiRepeat } from 'react-icons/fi'
 import { IoShirtOutline } from 'react-icons/io5';
 import { FaPlus } from 'react-icons/fa';
@@ -10,18 +10,30 @@ import logoImg from '../../assets/images/logo.jpg';
 import './styles.css';
 
 export default function Sidebar() {
+  const history = useHistory();
+
+  function handleLogout() {
+    localStorage.removeItem('@lavanderia:user');
+    localStorage.removeItem('@lavanderia:token');
+
+    history.push('/');
+    window.location.reload();
+  }
+
   return (
     <div className="sidebar">
-        <div className="div-logo">
-          <img src={logoImg} alt="logo" width="100%"></img>
-        </div>
-        
-        <Link to="/create-order" className="create-order"><FaPlus /><span>Criar Pedido</span></Link>
-        <Link to="/customers" className="options"><FiUsers size="18px"/> &nbsp; Clientes</Link>
-        <Link to="/items" className="options"><IoShirtOutline size="18px"/> &nbsp; Peças</Link>
-        <Link to="/orders" className="options"><FiEdit size="18px"/> &nbsp; Pedidos</Link>
-        <Link to="/processing" className="options"><FiRepeat size="18px"/> &nbsp; Processamento</Link>
-        <Link to="/payment" className="options"><GiMoneyStack size="18px"/> &nbsp; Não Pagos</Link>
+      <div className="div-logo">
+        <img src={logoImg} alt="logo" width="100%"></img>
       </div>
+      
+      <Link to="/create-order" className="create-order"><FaPlus /><span>Criar Pedido</span></Link>
+      <Link to="/customers" className="options"><FiUsers size="18px"/> &nbsp; Clientes</Link>
+      <Link to="/items" className="options"><IoShirtOutline size="18px"/> &nbsp; Peças</Link>
+      <Link to="/orders" className="options"><FiEdit size="18px"/> &nbsp; Pedidos</Link>
+      <Link to="/processing" className="options"><FiRepeat size="18px"/> &nbsp; Processamento</Link>
+      <Link to="/payment" className="options"><GiMoneyStack size="18px"/> &nbsp; Não Pagos</Link>
+      <p className="username">admin@lavanderia</p>
+      <p className="logout" onClick={handleLogout}>Sair</p>
+    </div>
   );
 }
