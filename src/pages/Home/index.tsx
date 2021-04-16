0fd163a717bce81337b8d869a4c20f12790e84c4
Natanael Freire <natan.nascimento.freire@gmail.com>
2021-04-15 22:12:28 -0300

    function para ping no server

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,30 @@ import Sidebar from '../../components/Sidebar';
 import LogoWithoutBg from '../../assets/images/logo-removebg.png';
 
 import './styles.css';
+import { useEffect } from 'react';
+import api from '../../services/api';
 
 export default function Home() {
+  useEffect(() => {
+    const storageUser = localStorage.getItem('@lavanderia:user');
+
+    async function pingApi(u: string) {
+      api.post('session', {
+        user: u
+      }).then(response => {
+        if (response.status === 200) {
+          console.log('Logado!')
+        }
+      });
+    }
+
+    if (storageUser) {
+      const user = JSON.parse(storageUser).name;
+
+      pingApi(user);
+    }
+  }, []);
+
   return (
     <div className="page-home">
 
